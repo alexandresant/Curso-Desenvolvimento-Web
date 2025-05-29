64ac01d96b86a277e300e8d1a263ec89cc22f169
Alexandre Buhrer <alexandre_sant1@hotmail.com>
2025-05-29 16:50:22 -0300

    Dia 10, Inicio com JavaScript

---
+++ AulasJavaScript/Dia10/script.js
@@ -0,0 +1,6 @@
+var estudante = "Engenharia de software"
+var idade = 33
+var nome = "Alexandre"
+
+console.log("Olá, meu nome é", nome, ", tenho", idade, "anos e estudo", estudante)
+window.alert("Olá, meu nome é " + nome + ", tenho " + idade + " anos e estudo " + estudante)
